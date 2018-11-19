9dd4dd9a9a34de98e0321a30b3fcb9c4aa27a6ba
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-19 12:24:54 +0800

    处理缓存问题

--- article_friendlink.dwt.php
+++ article_friendlink.dwt.php
@@ -8,28 +8,7 @@
 
 {insert name='page_header'}
 
-<div class="infomation">
-    <div class="wrap ">
-        <div class="info-header">
-            <div class="info-logo">
-                <a href="{$site_index}"> <img src="{$shop_logo}" alt="{$shop_name}"></a>
-            </div>
-            <div class="info-menu">
-                <ul>
-                    <li onclick="javascript:document.getElementById('shop_help').click();" class="help-center {if $smarty.get.c eq 'help' || empty($smarty.get.c) } current {/if}">
-                        <a id="shop_help" href='{url path="article/help/init"}'><span>帮助中心</span> </a>
-                    </li>
-                    <li onclick="javascript:document.getElementById('shop_notice').click();" class="new-ad {if $smarty.get.c eq 'notice'} current {/if}">
-                        <a id="shop_notice" href='{url path="article/notice/init"}'><span>商城公告</span></a>
-                    </li>
-                    <li onclick="javascript:document.getElementById('shop_info').click();" class="about-our {if $smarty.get.c eq 'info'} current {/if}">
-                        <a id="shop_info" href='{url path="article/info/init"}'> <span>关于我们</span></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
+
 <div class="banner help-banner">
     <div class="help-banner-wp">
         <a role="button" style="background:url('{$theme_url}images/banner.png') center center no-repeat;"></a>
