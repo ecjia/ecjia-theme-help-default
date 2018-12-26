dc14217e1231621a37b34c0b246163f80e30e075
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-26 11:43:16 +0800

    优化排序

--- extras/controller/article_controller.php
+++ extras/controller/article_controller.php
@@ -63,8 +63,8 @@ class article_controller
             $options = array(
                 'page_size'     =>  99999999,
                 'article_type'  => $article_type,
-                'sort_order'    => 'ASC',
-                'article_id'    => 'ASC',
+                'sort_order'    => 'DESC',
+                'sort_by'    => 'add_time',
             );
 
             $data = RC_Api::api('article', 'article_list', $options);
@@ -111,7 +111,7 @@ class article_controller
             $options = array(
                 'page_size'     =>  99999999,
                 'article_type'  => $article_type,
-                'sort_order'    => 'ASC',
+                'sort_order'    => 'DESC',
                 'sort_by'    => 'add_time',
             );
 
@@ -164,7 +164,7 @@ class article_controller
             $options = array(
                 'page_size'     =>  $page_size,
                 'article_type'  => $article_type,
-                'sort_order'    => 'ASC',
+                'sort_order'    => 'DESC',
                 'sort_by'    => 'add_time',
             );
 
@@ -215,7 +215,7 @@ class article_controller
             $options = array(
                 'page_size'     =>  $page_size,
                 'article_type'  => $article_type,
-                'sort_order'    => 'ASC',
+                'sort_order'    => 'DESC',
                 'sort_by'    => 'add_time',
             );
 
@@ -262,7 +262,7 @@ class article_controller
             $options = array(
                 'page_size'     =>  99999999,
                 'article_type'  => $article_type,
-                'sort_order'    => 'ASC',
+                'sort_order'    => 'DESC',
                 'sort_by'    => 'add_time',
             );
 
